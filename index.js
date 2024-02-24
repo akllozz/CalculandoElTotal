f0e2cc7e12c51b03292a9e0b79b4ef7028a564b5
Anakaren Lozano <aklozzg@gmail.com>
2024-02-24 13:08:20 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let precio = 400000;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+let count = 0;
+
+const cantidad = document.querySelector('.cantidad');
+let total = document.querySelector(".valor-total");
+
+function plus() {
+    count++;
+    cantidad.innerHTML = count;
+    total.innerHTML= Number(precio)*Number(count);
+}
+
+function minus(){
+    if(count !== 0) {
+       count--;
+       cantidad.innerHTML = count;
+       total.innerHTML= Number(precio)*Number(count);
+    } 
+}
+
+
+
+
